ad63cb6033c760e889857e263def8c92d76166cd
Sarah Choung <sarahjchoung@gmail.com>
2020-05-07 16:16:24 -0700

    POST New Grades

--- components/app.js
+++ components/app.js
@@ -7,6 +7,9 @@ class App {
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
     this.gradeForm = gradeForm;
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
   handleGetGradesError(error) {
     console.error(error);
@@ -32,6 +35,30 @@ class App {
     })
   }
   start() {
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+  }
+  createGrade(name, course, grade) {
+    console.log(name, course, grade);
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "POST",
+      data: {
+        "name": name,
+        "course": course,
+        "grade": grade
+      },
+      headers: {
+        "X-Access-Token": "DXv8RxGv"
+      },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError,
+    })
+  }
+  handleCreateGradeError(erorr) {
+    console.error(error)
+  }
+  handleCreateGradeSuccess() {
     this.getGrades();
   }
 }
